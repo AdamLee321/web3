1ec0be14502737834b3662c6117f15e3aea791d4
Adam lee <Adam lee>
2016-12-10 14:31:51 +0000

    DB Updates

--- Web3project_BookIT/Web3project_BookIT/Web3project_BookIT/Contact.aspx.cs
+++ Web3project_BookIT/Web3project_BookIT/Web3project_BookIT/Contact.aspx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data;
 using System.Configuration;
 using System.Data.SqlClient;
